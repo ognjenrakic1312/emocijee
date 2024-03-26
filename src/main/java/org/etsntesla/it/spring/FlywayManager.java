fde377d76bd03b18706c94a03aaa43b0002cd968
Nemanja Brkić <10221nemanjabrkic@etsntesla.edu.rs>
2024-03-26 10:03:29 +0100

    bla bla23456

--- src/main/java/org/etsntesla/it/spring/FlywayManager.java
+++ src/main/java/org/etsntesla/it/spring/FlywayManager.java
@@ -1,6 +1,7 @@
 package org.etsntesla.it.spring;
 
 
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,4 +18,16 @@ public class FlywayManager implements InitializingBean {
     public Flyway getFlyway() {
         return flyway;
     }
+}
+@Override
+public void afterPropertiesSet() throws Exception{
+        flyway = Flyway
+                .configure()
+                .dataSource(config.getUrl(), config.getUser(), config.getPass())
+                .locations("classpath:migrations")
+                .cleanDisabled(false)
+                .load();
+    }
+
+
 }
